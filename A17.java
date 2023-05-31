8b2ea2f5acbfb941b3544c40ed08599af671156c
AkshadaKharat <135003992+AkshadaKharat@users.noreply.github.com>
2023-05-31 18:14:33 +0530

    Add files via upload

---
+++ A17.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class  A17
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println(m1(1235));
+	}
+	public static int m1(int a)
+	{
+return (a/1000)+(a%10);
+
+}
+}
---
+++ Array1.java
@@ -0,0 +1,8 @@
+class Array1 
+{
+	public static void main(String[] args) 
+	{
+	int[]a1={1,2,3,3};
+	System.out.println(a1[3]);
+}
+}
