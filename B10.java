4e25aa7f2503aa09c81deeffa4136b51ee82a734
AkshadaKharat <135003992+AkshadaKharat@users.noreply.github.com>
2023-05-31 18:29:30 +0530

    Add files via upload

---
+++ B10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class  B9
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		double r=4;
+		double area=Math.PI*r*r;
+		double perimeter=2*Math.PI
+		System.out.println("Hello World!");
+	}
+}
